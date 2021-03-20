514c7c5b3d8438e678cab1d5600372a3ba2acc33
karenjennyli <karenjennyli@gmail.com>
2021-03-19 22:48:24 -0400

    cite code

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// started with code from: https://github.com/kristenkoyanagi/react-with-apis
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
